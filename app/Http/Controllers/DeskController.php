6d70e90cb655d9fe6f5d894a50fc3300e39bba23
omarsh99 <omar.sh990@gmail.com>
2023-06-20 23:45:31 +0200

    Created controller with stub functions

---
+++ app/Http/Controllers/DeskController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeskController extends Controller
+{
+    public function create(){
+        //display page for desk creation
+    }
+
+    public function store(){
+        //store desk
+    }
+
+    public function edit(){
+        //display page for desk edit
+    }
+
+    public function update(){
+        //update desk
+    }
+
+    public function destroy(){
+        //remove desk
+    }
+}
