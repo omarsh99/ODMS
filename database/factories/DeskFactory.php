559b49f3b32679e5f56d228b33a9df4c7af0b1f2
omarsh99 <omar.sh990@gmail.com>
2023-06-22 01:59:07 +0200

    Created Desk factory

---
+++ database/factories/DeskFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Desk>
+ */
+class DeskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'symbol' => Str::random(1),
+            'position_y' => 10,
+            'position_x' => 10,
+        ];
+    }
+}
