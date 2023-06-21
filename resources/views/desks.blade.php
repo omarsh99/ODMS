23441524eaf7f84111ef104b75bc08486f502298
omarsh99 <omar.sh990@gmail.com>
2023-06-21 22:03:45 +0200

    Added desks view

---
+++ resources/views/desks.blade.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" type="text/css" href="{{ url('/css/desks.css') }}" />
+
+<h1>All Desks</h1>
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($desks as $desk)
+            <tr>
+                <td>{{ $desk->name }}</td>
+                <td>{{ $desk->symbol }}</td>
+                <td>
+                    <a href="{{ route('desks.patch', $desk->id) }}">Edit</a>
+                </td>
+                <td>
+                    <form method="POST" action="{{ route('desks.destroy', $desk->id) }}" onsubmit="return confirm('Are you sure you want to delete this desk?')">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
