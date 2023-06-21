35e8865dc1f1e8839f3c67cc1352fa1062991e02
omarsh99 <omar.sh990@gmail.com>
2023-06-21 04:22:16 +0200

    Added delete button

--- resources/views/desk_edit.blade.php
+++ resources/views/desk_edit.blade.php
@@ -1,7 +1,14 @@
-<form method="POST" action="{{ route('desks.update', $desk->id) }}">
+<form method="POST" action="{{ route('desks.patch', $desk->id) }}">
     @csrf
     @method('PATCH')
     <input type="text" name="name" placeholder="Desk Name">
     <input type="text" name="symbol" placeholder="Symbol">
     <button type="submit">Update Desk</button>
+</form>
+
+<h1>Or delete</h1>
+<form method="POST" action="{{ route('desks.destroy', $desk->id) }}">
+    @csrf
+    @method('DELETE')
+    <button type="submit">Delete Desk!</button>
 </form>
