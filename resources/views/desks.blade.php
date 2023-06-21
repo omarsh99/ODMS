f5e1ab42cd641ebf1eb4896c3884526b36bb4d06
omarsh99 <omar.sh990@gmail.com>
2023-06-21 22:11:42 +0200

    bug fix  - route changed to desks.edit instead of desks.patch

--- resources/views/desks.blade.php
+++ resources/views/desks.blade.php
@@ -16,7 +16,7 @@
                 <td>{{ $desk->name }}</td>
                 <td>{{ $desk->symbol }}</td>
                 <td>
-                    <a href="{{ route('desks.patch', $desk->id) }}">Edit</a>
+                    <a href="{{ route('desks.edit', $desk->id) }}">Edit</a>
                 </td>
                 <td>
                     <form method="POST" action="{{ route('desks.destroy', $desk->id) }}" onsubmit="return confirm('Are you sure you want to delete this desk?')">
