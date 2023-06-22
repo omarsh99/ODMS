09771e0082354067bc19a2f35265c3b41167330e
omarsh99 <omar.sh990@gmail.com>
2023-06-22 16:02:49 +0200

    Added authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DeskController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DeskController::class, 'index']);
-Route::get('/desks', [DeskController::class, 'index']);
-Route::get('/desks/create', [DeskController::class, 'create']);
-Route::post('/desks', [DesKController::class, 'store']);
-Route::get('/desks/{id}/edit', [DeskController::class, 'edit'])->name('desks.edit');
-Route::put('/desks/{desk}', [DeskController::class, 'update'])->name('desks.update');
-Route::patch('/desks/{desk}', [DeskController::class, 'patch'])->name('desks.patch');
-Route::delete('/desks/{id}', [DeskController::class, 'destroy'])->name('desks.destroy');
+Route::get('/desks', [DeskController::class, 'index'])->middleware('isLoggedIn');
+Route::get('/desks/create', [DeskController::class, 'create'])->middleware('isLoggedIn');
+Route::post('/desks', [DesKController::class, 'store'])->middleware('isLoggedIn');
+Route::get('/desks/{id}/edit', [DeskController::class, 'edit'])->middleware('isLoggedIn')->name('desks.edit');
+Route::put('/desks/{desk}', [DeskController::class, 'update'])->middleware('isLoggedIn')->name('desks.update');
+Route::patch('/desks/{desk}', [DeskController::class, 'patch'])->middleware('isLoggedIn')->name('desks.patch');
+Route::delete('/desks/{id}', [DeskController::class, 'destroy'])->middleware('isLoggedIn')->name('desks.destroy');
+
+
+//User Routes
+Route::get('login', [AuthController::class, 'login'])->middleware('alreadyLoggedIn');
+Route::post('login', [AuthController::class, 'loginUser'])->name('login-user');
+Route::get('logout', [AuthController::class, 'logout'])->middleware('isLoggedIn');
+Route::get('register', [AuthController::class, 'register'])->middleware('alreadyLoggedIn');
+Route::post('register', [AuthController::class, 'registerUser'])->name('register-user');
+Route::get('profile', [AuthController::class, 'profile'])->middleware('isLoggedIn');
