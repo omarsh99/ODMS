425f868510378c7b413426f368a38c4a220098a2
omarsh99 <omar.sh990@gmail.com>
2023-06-22 17:04:48 +0200

    Auth Middleware does not run in testing env

--- app/Http/Middleware/AuthCheck.php
+++ app/Http/Middleware/AuthCheck.php
@@ -10,6 +10,10 @@ class AuthCheck
 {
     public function handle(Request $request, Closure $next): Response
     {
+        if (env('APP_ENV') === 'testing') {
+            return $next($request);
+        }
+
         if(!Session()->has('loginId')){
             return redirect('login')->with('fail', 'You have to be logged in to view this page!');
         }
